5e40ea9755072ee3d8fb597a6409cb700895c2ff
Pınar Kamit <pinarkamit@gmail.com>
2022-02-10 15:41:37 +0300

    extensionMethod exp

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ExtensionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+            bool result = x.IsEventNUmber();
+            Console.WriteLine(x + " is event number: " + result);
+        }
+    }
+
+    public static class MyExtensionClasss
+    {
+        public static bool IsEventNUmber(this int value)
+        {
+            return value%2==0;
+        }
+    }
+}
+
+
